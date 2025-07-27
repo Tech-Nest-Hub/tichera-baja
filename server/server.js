850f48afb55104886a7b8296be2a699430fb397d
Ghost9841 <asubedi778@gmail.com>
2025-07-27 12:40:37 +0545

    Sound Bhayo

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(cors({
   origin: 'http://localhost:5173', 
   credentials: true, 
 }));
-app.use('/uploads', express.static('uploads'));
+app.use('/api/uploads', express.static('uploads'));
 
 app.use('/api/sounds', soundRoutes);
 
